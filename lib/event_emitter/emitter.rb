d3d017743b62333e5ae3b166be10a12c116b2bbd
Sho Hashimoto <hashimoto@shokai.org>
2012-11-07 17:46:49 +0900

    EventEmitter#remove_listener  #3

--- lib/event_emitter/emitter.rb
+++ lib/event_emitter/emitter.rb
@@ -15,10 +15,24 @@ module EventEmitter
 
     def add_listener(type, &block)
       raise ArgumentError, 'listener block not given' unless block_given?
-      events << {:type => type.to_sym, :listener => block}
+      id = events.empty? ? 0 : events.last[:id]+1
+      events << {:type => type.to_sym, :listener => block, :id => id}
+      id
     end
 
     alias :on :add_listener
+
+    def remove_listener(id_or_type)
+      if id_or_type.class == Fixnum
+        events.delete_if do |e|
+          e[:id] == id_or_type
+        end
+      elsif [String, Symbol].include? id_or_type.class
+        events.delete_if do |e|
+          e[:type] == id_or_type.to_sym
+        end
+      end
+    end
     
     def emit(type, data)
       events.each do |e|
--- test/test_event_emitter.rb
+++ test/test_event_emitter.rb
@@ -33,4 +33,24 @@ class TestEventEmitter < MiniTest::Unit::TestCase
     assert result[:user] == 'shokai'
     assert result[:message] == 'hello world'
   end
+
+  def test_remove_listener
+    @foo.on :bar do |data|
+      puts "bar #{data}"
+    end
+    @foo.on :bar do |data|
+      puts "barbar: #{data}"
+    end
+
+    id = @foo.on :baz do |data|
+      p data
+    end
+
+    assert @foo.events.size == 3, 'check registerd listener count'
+    @foo.remove_listener id
+    assert @foo.events.size == 2, 'remove listener by id'
+
+    @foo.remove_listener :bar
+    assert @foo.events.size == 0, 'remove all "bar" listener'
+  end
 end
